553c84feb6c940292d03e8eeca36b09e7afa2c54
MarcusKhooLK <74861643+MarcusKhooLK@users.noreply.github.com>
2023-01-20 17:46:00 +0800

    split function

--- question1/src/main/java/com/assessment/telepathylabs/question1/Application.java
+++ question1/src/main/java/com/assessment/telepathylabs/question1/Application.java
@@ -21,41 +21,47 @@ public class Application {
         }
 
         try {
-            Scanner reader = new Scanner(new FileInputStream(args[0]));
-            List<Date> startTimes = new ArrayList<>();
-            List<Date> endTimes = new ArrayList<>();
-            DateFormat formatter = new SimpleDateFormat("kk:mm");
-            while (reader.hasNext()) {
-                String[] fromAndTo = reader.nextLine().split("-");
-                if (fromAndTo.length != 2) {
-                    continue;
-                }
-                startTimes.add(formatter.parse(fromAndTo[0]));
-                endTimes.add(formatter.parse(fromAndTo[1]));
-            }
-            Collections.sort(startTimes);
-            Collections.sort(endTimes);
-            int ongoingMeetings = 1;
-            int noOfMeetingRooms = 1;
-            int i = 1;
-            int j = 0;
-            while (i < startTimes.size() && j < endTimes.size()) {
-                if (startTimes.get(i).compareTo(endTimes.get(j)) < 0) {
-                    ongoingMeetings++;
-                    if (ongoingMeetings > noOfMeetingRooms) {
-                        noOfMeetingRooms = ongoingMeetings;
-                    }
-                    i++;
-                } else {
-                    ongoingMeetings--;
-                    j++;
-                }
-            }
+            int noOfMeetingRooms = findMeetingRoomsRequired(args[0]);
             System.out.println(noOfMeetingRooms);
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
+    }
+
+    private static int findMeetingRoomsRequired(String filePath) throws Exception {
+        Scanner reader = new Scanner(new FileInputStream(filePath));
+        List<Date> startTimes = new ArrayList<>();
+        List<Date> endTimes = new ArrayList<>();
+        DateFormat formatter = new SimpleDateFormat("kk:mm");
+        while (reader.hasNext()) {
+            String[] fromAndTo = reader.nextLine().split("-");
+            if (fromAndTo.length != 2) {
+                continue;
+            }
+            startTimes.add(formatter.parse(fromAndTo[0]));
+            endTimes.add(formatter.parse(fromAndTo[1]));
+        }
+        Collections.sort(startTimes);
+        Collections.sort(endTimes);
+        int ongoingMeetings = 1;
+        int noOfMeetingRooms = 1;
+        int i = 1;
+        int j = 0;
+        while (i < startTimes.size() && j < endTimes.size()) {
+            if (startTimes.get(i).compareTo(endTimes.get(j)) < 0) {
+                ongoingMeetings++;
+                if (ongoingMeetings > noOfMeetingRooms) {
+                    noOfMeetingRooms = ongoingMeetings;
+                }
+                i++;
+            } else {
+                ongoingMeetings--;
+                j++;
+            }
+        }
+
+        return noOfMeetingRooms;
     }
 
 }
